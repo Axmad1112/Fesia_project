224ae26a02ad50f5ea923429d42f540fd25276f6
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2021-12-17 18:14:11 +0500

    Profile CRUD create

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics,views
 from rest_framework.response import Response
-from .serializers import UserLoginSerializer, UserRegisterSerializer
-from .models import User
+from .serializers import UserLoginSerializer, UserRegisterSerializer, ProfileSerializer
+from .models import User, Profile
+
 
 class UserRegisterView(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -12,3 +13,18 @@ class UserLoginView(views.APIView):
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data)
+
+
+class ProfileRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+
+
+class ProfileUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+
+
+class ProfileDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
